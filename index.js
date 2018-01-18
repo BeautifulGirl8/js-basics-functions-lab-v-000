98a266302d85737187ef16591a7e3b896ebf5e79
Vicky  T <vttak9@gmail.com>
2018-01-18 05:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (customerLocation) {
+  if (customerLocation =< 42 )   {
+    const distanceFromHqInBlocks =`42 - ${customerLocation}`;
+ return `${distanceFromHqInBlocks} blocks.`;
+} else {
+    const distanceFromHqInBlocks = `&{customerLocation} - 42`;
+    return `${distanceFromHqInBlocks} blocks.`;
+} }
